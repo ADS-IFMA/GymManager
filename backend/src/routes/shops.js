e87f93fb115f4693b0e63b9df7a9b75916cfa772
Glaucon <viniciusglaucon@gmail.com>
2025-11-26 14:51:36 -0300

    Dashboard quase pronto - reorganizacao de pastas

--- backend/src/routes/shops.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getShops } from "../controllers/shopController.js";
-
-const router = express.Router();
-router.get("/", getShops);
-
-export default router;
