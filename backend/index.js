b9f1f1639114c992216d9b0190da153a7b82fead
Aniel_Nascimento <anielantonio063@gmail.com>
2026-03-30 10:42:54 -0300

    Implementar backend da autenticacao com mock

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const { login } = require('./services/authController');
+
+const app = express();
+
+app.use(cors()); 
+app.use(express.json());
+
+app.post('/api/login', login);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Endpoint de login disponível em: http://localhost:${PORT}/api/login`);
+});
